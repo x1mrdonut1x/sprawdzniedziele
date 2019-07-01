e7b33b5ad30c4af75cc59e0c1d9af528067ae329
Alex Niznik <alex@niznik.info>
2019-07-01 11:26:46 +0200

    Updates

---
+++ precache-manifest.6dcd993b1c671c510d09424a02c7a063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5620dccac7148764177751cc480d992d",
+    "url": "/sprawdzniedziele/index.html"
+  },
+  {
+    "revision": "188cd56c5db7226231ae",
+    "url": "/sprawdzniedziele/static/css/main.adab06fd.chunk.css"
+  },
+  {
+    "revision": "33feee8ca7fe509e595f",
+    "url": "/sprawdzniedziele/static/js/2.38793ced.chunk.js"
+  },
+  {
+    "revision": "188cd56c5db7226231ae",
+    "url": "/sprawdzniedziele/static/js/main.8c661cdc.chunk.js"
+  },
+  {
+    "revision": "39f10bc007d6a72fb069",
+    "url": "/sprawdzniedziele/static/js/runtime~main.eb1e93e6.js"
+  }
+]);
