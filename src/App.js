a5f5d8efd2fe9ba1fccedda4a4bc5f8e1c8a8e27
Alex Niznik <alex@niznik.info>
2019-07-01 11:22:55 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+import moment from "moment";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTodayTheLastSunday: this.isSunday(),
+      nextSunday: this.nextSunday(),
+      isSunday: moment().day() === 0
+    };
+  }
+  
+  componentWillMount() {
+    document.title = `Handlowa? ${this.renderText()}`
+  }
+  
+  nextSunday(){
+    return this.isSunday() ? moment().add(1, "months").endOf("month").day(0) : moment().endOf("month").day(0)
+  }
+
+  isSunday() {
+    let lastSunday = moment()
+      .endOf("month")
+      .day(0);
+      
+    let today = moment();
+    return today.isSame(lastSunday);
+  }
+
+  renderText(){
+    let { isTodayTheLastSunday } = this.state;
+    return isTodayTheLastSunday ? "TAK" : "NIE";
+  }
+
+  render() {
+    let { nextSunday, isSunday } = this.state;
+    return (
+      <div className="App">
+        <div className="Header">Czy dzisiaj jest niedziela handlowa?</div>
+        <div className="Text">{this.renderText()}</div>
+        {isSunday ? null : <div className="Subheader">ale dzisiaj zrobisz zakupy!</div>}
+        <div className="NextSundayWrapper">
+          Następna niedziela handlowa:{" "}
+          <div className="NextSunday">{nextSunday.format("DD-MM-YYYY")} </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import moment from 'moment'
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('checks if last sunday', () => {
+  let mockState = {
+    isTodayTheLastSunday: true,
+    isSunday: true
+  }
+  const wrapper = shallow(<App />);
+  wrapper.setState({ ...mockState });
+  const text = wrapper.find('.Text'); 
+  expect(text.text()).toEqual('TAK');
+});
+
+it('checks if not last sunday', () => {
+  let mockState = {
+    isTodayTheLastSunday: false,
+    isSunday: true
+  }
+  const wrapper = shallow(<App />);
+  wrapper.setState({ ...mockState });
+  const text = wrapper.find('.Text'); 
+  const subtext = wrapper.find('.Subheader'); 
+
+  expect(text.text()).toEqual('NIE');
+  expect(subtext).toEqual({});
+});
+
+it('checks if not last sunday but day of week', () => {
+  let mockState = {
+    isTodayTheLastSunday: false,
+    isSunday: false
+  }
+  const wrapper = shallow(<App />);
+  wrapper.setState({ ...mockState });
+  const text = wrapper.find('.Text'); 
+  const subtext = wrapper.find('.Subheader');
+
+  expect(text.text()).toEqual('NIE');
+  expect(subtext.text()).toEqual('ale dzisiaj zrobisz zakupy!');
 });
